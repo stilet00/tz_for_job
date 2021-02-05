bb1d1e02651260d70f0aa1ea22922325971e0587
stilet00 <safroninanton@gmail.com>
2021-02-05 16:32:17 +0200

    loader works

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -102,7 +102,9 @@
 
         </div>
 
-
+        <div class="lds-spinner"
+        v-if="sending"
+        ><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
         <SingUpButton
         type="submit"
         inner="Sing up now"
@@ -133,12 +135,14 @@ export default {
         photo: null,
         validation: false,
         token: null,
-      }
+      },
+      sending: false
     }
   },
   methods: {
     getToken() {
       if (this.user.validation) {
+        this.sending = true
         axios
             .get('https://frontend-test-assignment-api.abz.agency/api/v1/token')
             .then(res => {
@@ -236,6 +240,7 @@ export default {
       this.user.photo = null
       this.user.validation = false
       this.user.token = null
+      this.sending = false
     },
     regDone() {
       this.$emit('success')
@@ -322,6 +327,81 @@ export default {
 .err-shown
   opacity: 1!important
 
+//spinner
+.lds-spinner
+  position: absolute
+  display: inline-block
+  width: 80px
+  height: 80px
+  bottom: 40%
 
+  div
+    transform-origin: 40px 40px
+    animation: lds-spinner 1.2s linear infinite
 
+    &:after
+      content: " "
+      display: block
+      position: absolute
+      top: 3px
+      left: 37px
+      width: 6px
+      height: 18px
+      border-radius: 20%
+      background: #fdd
+
+    &:nth-child(1)
+      transform: rotate(0deg)
+      animation-delay: -1.1s
+
+    &:nth-child(2)
+      transform: rotate(30deg)
+      animation-delay: -1s
+
+    &:nth-child(3)
+      transform: rotate(60deg)
+      animation-delay: -0.9s
+
+    &:nth-child(4)
+      transform: rotate(90deg)
+      animation-delay: -0.8s
+
+    &:nth-child(5)
+      transform: rotate(120deg)
+      animation-delay: -0.7s
+
+    &:nth-child(6)
+      transform: rotate(150deg)
+      animation-delay: -0.6s
+
+    &:nth-child(7)
+      transform: rotate(180deg)
+      animation-delay: -0.5s
+
+    &:nth-child(8)
+      transform: rotate(210deg)
+      animation-delay: -0.4s
+
+    &:nth-child(9)
+      transform: rotate(240deg)
+      animation-delay: -0.3s
+
+    &:nth-child(10)
+      transform: rotate(270deg)
+      animation-delay: -0.2s
+
+    &:nth-child(11)
+      transform: rotate(300deg)
+      animation-delay: -0.1s
+
+    &:nth-child(12)
+      transform: rotate(330deg)
+      animation-delay: 0s
+
+@keyframes lds-spinner
+  0%
+    opacity: 1
+
+  100%
+    opacity: 0
 </style>
