0b74c97a853549d56750ebc1f3db4317b5f94422
stilet00 <safroninanton@gmail.com>
2021-02-13 23:27:49 +0200

    button added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,73 @@
 <template>
-    <header>
-      <div class="logo">
-        <img src="../assets/favicon-32x32.png" alt="">
-        TESTTASK
-      </div>
-      <nav>
+  <header>
+    <div class="logo">
+      <img src="../assets/favicon-32x32.png" alt="">
+      TESTTASK
+    </div>
+    <button
+        @click="showHide"
+        v-if="smartPhone"
+    >
+      <img src="../assets/menu_icon.svg" alt="">
+    </button>
+    <transition v-on:before-enter="beforeEnter"
+                v-on:enter="enter"
+                v-on:leave="leave"
+                v-bind:css="false">
+      <nav id="nav-1" v-if="show">
         <a href="#registration">About me</a>
         <a href="#registration">Relationships</a>
         <a href="#registration">Requirements</a>
         <a href="#registration">Users</a>
         <a href="#registration">Sing up</a>
       </nav>
+    </transition>
 
-    </header>
+
+  </header>
 
 </template>
 
 <script>
+import Velocity from 'velocity-animate'
 export default {
-name: "Header"
+  name: "Header",
+  data() {
+    return {
+      smartPhone: false,
+      show: true
+    }
+  },
+  methods: {
+    showHide() {
+      this.show = !this.show
+    },
+    beforeEnter: function (el) {
+      el.style.opacity = 0
+      el.style.transformOrigin = 'left'
+    },
+    enter: function (el, done) {
+      Velocity(el, { opacity: 1, translateY: '20px'}, { duration: 300 })
+      Velocity(el, { fontSize: '12px', translateY: '0px' }, { complete: done })
+    },
+    leave: function (el, done) {
+      Velocity(el, { translateX: '15px' }, { duration: 600 })
+      Velocity(el, {
+        opacity: 0
+      }, { complete: done })
+    }
+  },
+  mounted() {
+    if (window.screen.width < 500) {
+      this.smartPhone = true;
+      this.show = false;
+    } else {
+      this.smartPhone = false;
+    }
+  }
 }
 </script>
+
 
 <style lang="sass" scoped>
 header
@@ -33,6 +80,8 @@ header
   box-sizing: border-box
   padding: 5px 20px
   max-width: 1170px
+  button
+    margin: 0 auto
 nav
   display: flex
   align-items: center
@@ -66,6 +115,7 @@ nav
     nav
       font-size: 10px
       display: flex
+      flex-direction: column
       justify-content: space-evenly
       width: 100%
       a
@@ -73,7 +123,14 @@ nav
 
 
 
+//animate
 
+.fade-enter-active, .fade-leave-active
+  transition: opacity 1s ease-out
+
+.fade-enter, .fade-leave-to
+  opacity: 0
+  transform: translatey(-20px)
 
 
 </style>
